9b5ae7dba7f96cc0df0e4ee305d481c73003d827
nastya13 <nastya94-94.08@mail.ru>
2015-03-28 16:59:27 +0300

    Task completed

--- src/Main.hs
+++ src/Main.hs
@@ -35,23 +35,23 @@ high_of_bstree bst = let
 sum_elem :: BStree -> Int
 sum_elem bst = let
                   go bst' sum = if bst' /= empty_tree
-								      then (value bst') + go (bstl bst') 0 + go (bstr bst') 0
-								  else sum
-			   in go bst 0
+                                    then (value bst') + go (bstl bst') 0 + go (bstr bst') 0
+                                else sum
+               in go bst 0
 
 -------------------------------------------------------------
 --finding element in binary search tree
 find_elem :: BStree -> Int -> String
 find_elem bst elem = let
                         go bst' res = if (value bst') == elem
-									      then (res ++ show (value bst'))
-									  else
-									      if elem < (value bst')
-										      then go (bstl bst') (res ++ show (value bst') ++ ", Left ")
-										  else
-										      go (bstr bst') (res ++ show (value bst') ++ ", Right ")
+                                          then (res ++ show (value bst'))
+                                      else
+                                          if elem < (value bst')
+                                              then go (bstl bst') (res ++ show (value bst') ++ ", Left ")
+                                          else
+                                              go (bstr bst') (res ++ show (value bst') ++ ", Right ")
 											  
-					 in go bst "root "
+                     in go bst "root "
 
 
 
